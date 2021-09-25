4c90de8a35b7a7202e42ca58d766a8d93e12e583
Gavril Ioan Hasmasan <gsylverman@gmail.com>
2021-09-26 00:11:31 +0300

    added sample repositories

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -14,10 +16,12 @@ public class BootstrapData  implements ApplicationListener<ContextRefreshedEvent
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -30,14 +34,25 @@ public class BootstrapData  implements ApplicationListener<ContextRefreshedEvent
         Author eric = new Author("Eric", "Land");
         Book someBook = new Book("Programming Book", "12133131");
 
+        Publisher publisher = new Publisher();
+        publisher.setName("publisher X");
+        publisher.setCity("NY");
+        publisher.setState("NY");
+
         eric.getBooks().add(someBook);
         someBook.getAuthors().add(eric);
+        someBook.setPublisher(publisher);
+
+        publisherRepository.save(publisher);
+
+        publisher.getBooks().add(someBook);
 
         authorRepository.save(eric);
         bookRepository.save(someBook);
 
         Author john = new Author("John", "Evans");
         Book bookXs = new Book("XS", "12123");
+        publisher.getBooks().add(bookXs);
 
         john.getBooks().add(bookXs);
         bookXs.getAuthors().add(john);
@@ -47,6 +62,8 @@ public class BootstrapData  implements ApplicationListener<ContextRefreshedEvent
 
 
         System.out.println("from guru.springframework.spring5webapp.bootstrap.............");
-//        System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Publisher number of books: " + publisher.getBooks().size());
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -11,7 +11,9 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @ManyToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -26,7 +28,7 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -57,11 +59,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
